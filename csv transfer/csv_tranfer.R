50b0e9e7ff1b7f29a7cf46336103bda42b624166
Confidence Mothiba <mothibacs@gmail.com>
2024-02-23 21:03:10 +0200

    Update csv_tranfer.R

--- csv transfer/csv_tranfer.R
+++ csv transfer/csv_tranfer.R
@@ -1,37 +1,61 @@
+library(rsconnect)
 library(shiny)
 library(haven)
 library(DT)
+library(foreign)
+library(RODBC)
+library(htmltools)
+library(shinyWidgets)
+
+rsconnect::setAccountInfo(name='iedea-dt', token='B64519710E2C403E20FCDB812C58F9EF', secret='i/XRXO6PnJghPX4jicVKjfqtdB3P5kEyAXrlDdsV')
+
+#install.packages(update=TRUE)
 
 #incrse file upload size to 2GB
-options(shiny.maxRequestSize = 2147483648)
+options(shiny.maxRequestSize = 5 * 1024^3)
 # Define UI ----
 ui <- fluidPage(
-  titlePanel(span(" IeDEA", style= "color: darkred", span(" .sasdbat transfer to CSV", ))),
+  tags$head(
+    tags$link(rel="IeDEA-DT", href="favicon.ico"),
+    tags$title("IeDEA Data Transfer Toolkit")
+  ),
+  #titlePanel("IeDEA -DT"),
+  br(),
   sidebarLayout(
     sidebarPanel(
-      img(src = "logo2.png", height = 100, width = 500),
-      h3("CSV Transfer is used as an alternative tool to transfer .sas7bdat datasets to a .csv file."),
-      h4("IeDEA Tables"),
-      h5("tblART"),
-      h5("tblBAS"),
-      h5("And more...")
+      img(src = "logo2.png", height = 150, width = "100%"),
+      h3("IeDEA Dataset Transfer Toolkit is a tool used for transfering datasets to a csv format. It accepts
+         files like sas, dta and txt files"),
+      br(),
+      
     ),
     mainPanel(
       fluidRow(
-        column(4,
-               fileInput("file", h3("Select file to transfer")),
-               textInput("text", h3("Provide file name"), placeholder = "Enter file name..."),
-               actionButton("export_csv", "Export to CSV", style = "color: white; background-color: blue;"),
-               br()
-        )
-      ),
-      
-      fluidRow(
-        br(),
-        column(12,
-               DTOutput("table")  # Moved DTOutput inside the mainPanel
+        column(6,
+               wellPanel(
+                 #div(class="box-header box box-solid box-success", h3("File Transer")),
+                 fileInput("file", HTML("Upload file to transfer: Max 5GB<br>
+                    Allowed file formats: <span class='badge badge-fix' style='background-color: blue'>sas</span>
+                                        <span class='badge badge-fix' style='background-color: maroon'>dta</span>
+                                        <span class='badge badge-fix' style='background-color: white; color: black'>txt</span>"), multiple=TRUE),
+                 textInput("text", h4("Provide file name")),
+                 actionButton("export_csv", "Export to CSV", class="btn-success"),
+                 br()
+               )
+        ),
+        column(6,
+               wellPanel(
+                 h4("Summary of Uploaded IeDEA Table"),
+                 tableOutput("summary_table")
+               )
         )
       )
+    )
+  ), #end of sidelayout
+  fluidRow(
+    column(12,
+           h3("Table output summary"),
+           DTOutput("table")
     )
   )
 )
@@ -39,31 +63,64 @@ ui <- fluidPage(
 # Define server logic ----
 server <- function(input, output, session) {
   data <- reactive({
-    if (!is.null(input$file)) {
+    req(input$file)
+    if (endsWith(input$file$name, ".dta")) {
+      haven::read_dta(input$file$datapath)
+    } else if (endsWith(input$file$name, ".sas7bdat")) {
       haven::read_sas(input$file$datapath)
+    } else if(endsWith(input$file$datapath, ".txt")) {
+      read.table(input$file$datapath, header=TRUE, sep=",")
+    }
+      else{NULL}
+  })
+  
+  output$summary_table <- renderTable({
+    
+    if (!is.null(data())) {
+      filename <- input$file$name
+      tablename <- sub("\\.\\w+$", "", filename) # Extracting table name from filename
+      total_records <- nrow(data()) # Total number of records
+      column_names <- colnames(data()) # Column names
+      
+      #table_html <- paste(HTML("<span class='badge badge-fix'>", tablename, "</span>", sep = ""))
+      summary_data <- data.frame("Table" = tablename,
+                                 "Records" = total_records,
+                                 "Columns" = paste(column_names, collapse = ", "))
+      summary_data
     }
   })
   
   output$table <- renderDT({
     datatable(
       data(),
-      options = list(pageLength = input$rows_per_page)
+      options = list(
+        pageLength = input$rows_per_page,
+        dom = 't<"dt-custom-search"i>p'
+      )
     )
   })
   
   observeEvent(input$export_csv, {
-    if (!is.null(input$file)) {
-      withProgress(message = 'Exporting CSV...', value = 0, {
-        # Simulating a long process
-        for (i in 1:15) {
-          incProgress(1/15)
-          Sys.sleep(0.2)
-        }
-        write.csv(data(), file = paste0(input$text, ".csv"), row.names = FALSE)
-        # Clear the text input field after export
-        updateTextInput(session, "text", value = "")
-      })
-    }
+    req(input$file)
+    withProgress(message = 'Exporting Data...', value = 0, {
+      # Simulating a long process
+      for (i in 1:15) {
+        incProgress(1/15)
+        Sys.sleep(0.2)
+      }
+      
+      write.csv(data(), file = paste0(input$text, ".csv"), row.names = FALSE)
+      showModal(
+        modalDialog(
+          title="Success!",
+          "File exported Successfully",
+          easyClose = TRUE
+        )
+      )
+      # Clear the text input field after export
+      updateTextInput(session, "text", value = "")
+    })
+    
   })
 }
 
